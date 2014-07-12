d1731dc9cb4a7fe103701b7601f08ad1fb536050
Absi Oussama <oussamaabsi007@gmail.com>
2014-07-12 03:37:45 +0000

    plot1

--- plot1.R
+++ plot1.R
@@ -3,4 +3,5 @@ colclasses = c("character", "character", rep("numeric",7))
 data <- read.table(file="household_power_consumption.txt",header=T,sep=";", colClasses = colclasses, na.strings=c("?"))
 data<-subset(data, data$Date == "1/2/2007" | data$Date == "2/2/2007")
 hist(as.numeric(type.convert(as.character(data$Global_active_power), dec = ".")), main = "Global Active Power", breaks = 14, xlab = "Global Active Power (kilowatts)", col = "red", xaxp = c(0, 6, 3))
-## saving the plot as image (480x,480x)
+## saving the plot as image (480x,480x) png
+ 
